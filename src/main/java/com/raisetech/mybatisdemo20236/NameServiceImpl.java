8fff30ee6a6d6e227354bcff6c20b333c984b777
ebiharahiroki <laoyuanhonggui@gmail.com>
2023-06-05 06:49:34 +0900

    READMEと大文字に変換、構成のフォーマットの仕方が不明

--- src/main/java/com/raisetech/mybatisdemo20236/NameServiceImpl.java
+++ src/main/java/com/raisetech/mybatisdemo20236/NameServiceImpl.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class NameServiceImple implements NameService {
+public class NameServiceImpl implements NameService {
 
     private final NameMapper nameMapper;
 
-    public NameServiceImple(NameMapper nameMapper) {
+    public NameServiceImpl(NameMapper nameMapper) {
         this.nameMapper = nameMapper;
     }
 
--- src/main/java/com/raisetech/mybatisdemo20236/README.md
+++ src/main/java/com/raisetech/mybatisdemo20236/README.md
@@ -10,3 +10,4 @@
 └── service
 ├── NameService.java
 └── NameServiceImpl.java
+
